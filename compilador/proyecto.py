6091d63969184d6d1c254fc567b018eda3f6eb10
unknown <xeniaalfaro99@hotmail.com>
2022-05-18 21:45:00 -0600

    Se agregan los archivos al repositorio.

---
+++ compilador/proyecto.py
@@ -0,0 +1,69 @@
+from logging import error
+import funciones
+import sympy
+from graphviz import Source
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
+
+from sympy.parsing.sympy_parser import parse_expr
+
+answerResult = {
+    "integer": [],
+    "decimal": [],
+    "operators": [],
+    "parenthesis": False,
+    "answer": 0,
+}
+positionTokenElement = []
+matematicalOperationString = input('por favor ingrese una operacion matematica ejemplo: 2*3(5/2)\n')
+try:
+    splittedString = funciones.splitString(matematicalOperationString)
+    # funciones de analizador lexico
+    allnumbers = funciones.getAllNumbers(matematicalOperationString)
+    (answerResult["decimal"], answerResult['integer']) = funciones.isFloatorNot(allnumbers)
+    answerResult['operators'] = set(funciones.getOperators(matematicalOperationString))
+    answerResult['parenthesis'] = funciones.detectParenthesis(matematicalOperationString)
+    # funciones analizador sintactico
+    (position,Token,Element)=funciones.createListPositionTokenElement(splittedString)
+    positionTokenElement.append(position)
+    positionTokenElement.append(Token)
+    positionTokenElement.append(Element)
+    # funciones analizador sintactico
+    answerResult['answer'] = eval(matematicalOperationString)
+    print(' resultados => ',answerResult,' posicion, tipo de token, valor => ',positionTokenElement)
+    ##resolviendo las operaciones en parenthesis
+    (arrayOperationParenthesis, answer) = funciones.getParenthesisOperations(splittedString)
+    for i in range(len(arrayOperationParenthesis)):
+        matematicalOperationString = matematicalOperationString.replace(arrayOperationParenthesis[i],str(answer[i]))
+    
+
+
+except:
+    if  positionTokenElement[2][0] == '*' or positionTokenElement[2][0] == '/':
+        print('la cadena no de debe de empezar con los operadores / *')
+    for el in positionTokenElement[0]:
+            if positionTokenElement[1][el-1] == 'operador' :
+                if positionTokenElement[1][el] == 'operador' :
+                    print('no deben de haber dos operadores seguidos error en la posicion', el)
+            if positionTokenElement[1][el-1] == 'parentesis' :
+                if positionTokenElement[1][el-2] != 'operador' or positionTokenElement[1][el] != 'operador'  :
+                    print('antes o despues de los parentesis debe de haber un operador, error en la posicion ', el)
+
+
+
+
+
+
+convertedExpression = parse_expr(matematicalOperationString)
+otro = sympy.srepr(convertedExpression)
+
+print(convertedExpression)
+
+#src = Source(sympy.dotprint(parse_expr(matematicalOperationString)))
+a, b, c, d = sympy.symbols('6, 2, 3, 4')
+expr = (a+b/-c)+b-d
+
+src = Source(sympy.dotprint(expr), format='png')
+src.render('output.png', view=True)
+
+
